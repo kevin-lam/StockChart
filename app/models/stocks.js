98589a1fa8d9931d2c34cb9439d4da6a05c3a06a
kevin-lam <kevin.lam.cs@gmail.com>
2018-01-02 15:05:24 -0800

    Database connection logging messages

--- app/models/stocks.js
+++ app/models/stocks.js
@@ -4,8 +4,11 @@ require('dotenv').config();
 var pg = require('pg');
 
 var uri = process.env.DATABASE_URL; 
+console.log("uri: " + uri);
 var client = new pg.Client(uri);
 client.connect();
+console.log("client connected");
 client.query('CREATE TABLE stocks(symbol TEXT PRIMARY KEY NOT NULL, dates TEXT[], open REAL[])', function (err,res) {
 	client.end();
+  console.log("table created");
 });
